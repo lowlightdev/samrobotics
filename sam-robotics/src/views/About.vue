122b8638112286a1cc770746f343c5f619fee0b6
zapo <contact@victorz.me>
2020-07-13 02:52:32 +0100

    redesign

---
+++ sam-robotics/src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="about row align-items-center vh-100 text-center no-gutters">
+    <div class="col-sm-12 col-md-6 animate__slideInLeft animate__animated background--red  text-white vh-100 d-flex align-items-center">
+      <font-awesome-icon icon="arrow-left" class="align-self-start justify-self-start m-4 fa-2x position-absolute" v-on:click="goHome"/>
+      <h1 class="m-auto animate__fadeIn animate__animated">{{ $route.name }}</h1>
+    </div>
+    <div class="col-sm-12 col-md-6 animate__slideInRight animate__animated">
+      <h2 class="animate__fadeIn animate__animated"> Despre SAM Robotics</h2>
+      <p class="text-left p-4 animate__fadeIn animate__animated"> Societatea SAM Robotics este reprezentantul oficial din 2008, in Romania pentru: Yaskawa Robotics Division (MOTOMAN) care este unul din liderii mondiali in conceperea, productia, implementarea si comercializarea de celule robotizate si roboti industriali cu diverse domenii de aplicabilitate in industrie, cercetare si invatamint. Supunem atentiei dumneavoastra aceasta gama de servicii si produse, prin statulul nostru de distribuitor autorizat in Romania. Pe langa aceasta colectivul nostru va ofera si o intreaga gama de echipamente, unelte si accesorii necesare sectorului de confectii si constructii metalice. </p>
+    </div>
+    <PageFooter />
+  </section>
+</template>
+
+<script>
+import PageFooter from "@/components/PageFooter";
+
+export default {
+  name:'about',
+  components: {
+    PageFooter
+  },
+  methods:{
+    goHome(){
+      this.$router.push('/')
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.fa-arrow-left{
+  cursor: pointer !important;
+}
+.vh-100{
+  @media screen and (max-width: 960px){
+    max-height:250px !important; 
+  }
+}
+</style>
---
+++ sam-robotics/src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <main class="container-fluid vh-100 d-flex animate__animated animate__zoomIn flex-column justify-content-center">
+    <section class="align-self-center m-auto border border-grey">
+      <div class="row no-gutters align-items-center justify-content-center text-center">
+        <div class="col block logo">
+        </div>
+        <div class="col block background--red"></div>
+        <div class="col block ">
+          <router-link to="about"  class="text-center mr-auto ml-auto font-weight-bold">Gama de produse</router-link>
+        </div>
+      </div>
+      <div class="row no-gutters align-items-center  justify-content-center text-center">
+        <div class="col block background--blue"></div>
+        <div class="col block">
+          <router-link to="about" class="text-center mr-auto ml-auto">
+            Despre noi
+          </router-link>
+        </div>
+        <div class="col block background--red"></div>
+      </div>
+      <div class="row no-gutters align-items-center justify-content-center text-center">
+        <div class="col block">
+          <router-link to="about"  class="text-center mr-auto ml-auto font-weight-bold">Blog</router-link>
+        </div>
+        <div class="col block background--blue"></div>
+        <div class="col block ">
+          <router-link to="about"  class="text-center mr-auto ml-auto font-weight-bold">Contact</router-link>
+        </div>
+      </div>
+    </section>
+    <PageFooter />
+  </main>
+</template>
+
+<script>
+import PageFooter from "@/components/PageFooter";
+
+export default {
+  name: 'home',
+  components: {
+    PageFooter
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+main{
+  @media screen and (max-width: 960px){
+    margin-top:100px;
+  }
+}
+.logo{
+  background-image: url('~@/assets/sam_logo.jpg');
+  background-size:contain;
+  background-repeat:no-repeat;
+  background-position: center center;
+}
+.border-grey{
+   border: 5px solid rgb(112, 112, 112);
+}
+</style>
